ec5e52320a7a068129504fd10250b4f4af2e6c95
zach <zaclewis@gmail.com>
2023-12-26 12:20:37 -0600

    update2

---
+++ client/src/main/java/zac/demo/FrontendApplication.java
@@ -0,0 +1,13 @@
+package zac.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+
+}
